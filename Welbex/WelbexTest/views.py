f78a046c751af51cbaf75cf32c7713cf29ba4ac0
artj15 <artj15@gmail.com>
2021-06-01 11:55:04 +0300

    WelbeX

--- Welbex/WelbexTest/views.py
+++ Welbex/WelbexTest/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .models import Table, TableFrom
+
+
 # Create your views here.
 def TableView(request):
     table = Table.objects.all()
     data = {
         'table': table
     }
-    return render(request, 'table.html', context= data)
+    return render(request, 'table.html', context=data)
