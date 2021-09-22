595090520cefaab0f6a58ec38ec8d446542110b1
artj15 <artj15@gmail.com>
2021-09-22 19:58:06 +0300

    WelbeX

--- Welbex/WelbexTest/models.py
+++ Welbex/WelbexTest/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 from django.forms import ModelForm
 
+
 # Create your models here.
 class Table(models.Model):
     data = models.DateField()
-    name = models.CharField(max_length= 1000)
+    name = models.CharField(max_length=1000)
     number = models.IntegerField()
-    distance = models.CharField(max_length= 1000)
+    distance = models.CharField(max_length=1000)
+
     class Meta:
         ordering = ['data']
-        verbose_name='WelbeXTable'
+        verbose_name = 'WelbeXTable'
+
     def __str__(self):
         return f'{self.name} {self.data}'
+
+
 class TableFrom(ModelForm):
     class Meta:
         model = Table
